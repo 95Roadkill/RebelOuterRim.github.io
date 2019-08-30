2e64df32aee53bdff475f4d6c8ea0d27feef58ba
95Roadkill <Lt.roadkill@yahoo.co.uk>
2019-08-30 14:23:27 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -22,7 +22,7 @@ var l_displayMapGamemode = true;
 
 // Custom server name configuration
 // Leave it blank for automatic server name
-var l_serverName = "sleekLoad 2";
+var l_serverName = "Outer Rim";
 
 // Use a video for the background?
 // Otherwise it will use images
@@ -113,8 +113,6 @@ var l_musicDisplay = true;
 // (Place the ogg files in the music folder)
 // (You can also use external URL for the ogg files)
 var l_musicPlaylist = [
-	{ogg: "lullaby.ogg", name: "Lullaby of Deserted Hell"},
-	{ogg: "falls_in_fall.ogg", name: "秋に舞う椛と滝飛沫"},
 	{youtube: "https://www.youtube.com/watch?v=3VTwbEWW2W8", name: "Tea for Two"},
 	{youtube: "https://www.youtube.com/watch?v=VaDN_19rPyw", name: "Voile, the Magic Library"},
 ];
